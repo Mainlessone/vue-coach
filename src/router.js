25132b90809823dad0bb8b4e29a786605dfab6f9
Mainlessone <syi.nrgy@gmail.com>
2022-06-07 13:15:26 +0300

    project init(structure + defined routes)

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import NotFoundPage from './pages/NotFoundPage.vue';
+import RequestsPage from './pages/requests/RequestsPage.vue';
+import RegistrationPage from './pages/registration/RegistrationPage';
+import CoachesListPage from './pages/coaches/CoachesListPage';
+import CoachDetailsPage from './pages/coaches/CoachDetailsPage';
+import ContactCoachPage from './pages/coaches/ContactCoachPage';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', redirect: '/coaches'}, 
+      { path: '/coaches', component: CoachesListPage },
+      { path: '/coaches/:id', component: CoachDetailsPage, children: [
+          { path: 'contact', component: ContactCoachPage }
+        ] 
+      },
+      { path: '/register', component: RegistrationPage },
+      { path: '/requests', component: RequestsPage },
+      { path: '/:notFound(.*)', component: NotFoundPage }
+    ]
+});
+
+export default router;
